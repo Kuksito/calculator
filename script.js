2308c8792618e12282d64362edbff271d6f797fb
karina <123593292+Kuksito@users.noreply.github.com>
2024-09-02 08:19:08 +0300

    Declare all btns and result containers

---
+++ script.js
@@ -0,0 +1,6 @@
+const selectionBtns = document.querySelectorAll('[data-selection]');
+const undoBtn = document.querySelector('[data-undo]');
+const clearBtn = document.querySelector('[data-clear]');
+const currentResultContainer = document.querySelector('#current-result');
+const pastResultContainer = document.querySelector('#past-result');
+
