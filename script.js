e3e5b98c983193123713d3227c74ebd48865c25a
karina <123593292+Kuksito@users.noreply.github.com>
2024-09-02 09:16:48 +0300

    Add equalBtn, add func for showing selected button

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const selectionBtns = document.querySelectorAll('[data-selection]');
 const undoBtn = document.querySelector('[data-undo]');
 const clearBtn = document.querySelector('[data-clear]');
+const equalBtn = document.querySelector('[data-equal]');
 const currentResultContainer = document.querySelector('#current-result');
 const pastResultContainer = document.querySelector('#past-result');
 
+selectionBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        const selectionBtnName = btn.dataset.selection;
+        displayClickedBtn(selectionBtnName);
+    })
+});
+
+function displayClickedBtn(clickedBtn){
+    currentResultContainer.textContent += clickedBtn;
+}
