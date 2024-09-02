c95320e7696f1cde2ef78635bf94d47b4580ee36
karina <123593292+Kuksito@users.noreply.github.com>
2024-09-02 10:19:39 +0300

    Add clear and undo funcs (on 'click')

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const equalBtn = document.querySelector('[data-equal]');
 const currentResultContainer = document.querySelector('#current-result');
 const pastResultContainer = document.querySelector('#past-result');
 
+let userRequest = [];
+
+clearBtn.addEventListener('click', () => {
+    currentResultContainer.textContent = '';
+});
+
+undoBtn.addEventListener('click', () => {
+    userRequest.splice(-1,1);
+    currentResultContainer.textContent = userRequest.join('');
+    console.log(userRequest)
+});
+
 selectionBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
         const selectionBtnName = btn.dataset.selection;
@@ -13,5 +25,11 @@ selectionBtns.forEach((btn) => {
 });
 
 function displayClickedBtn(clickedBtn){
-    currentResultContainer.textContent += clickedBtn;
-}
+    currentResultContainer.append(userRequest);
+    userRequest.push(clickedBtn);
+    currentResultContainer.textContent = userRequest.join('');
+
+    console.log(userRequest)
+    // console.log(userRequest.join(''));
+    // console.log(Array.isArray(userRequest))
+};
